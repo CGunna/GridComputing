e5315e8893f0c1f1b7f956c1dcbff8b051a88dbe
Gunter Wiesinger <g_wiesinger92@hotmail.com>
2015-10-14 10:12:45 +0200

    Testcommit

---
+++ GridComputing/ExecutableTransmitter/Program.cs
@@ -0,0 +1,32 @@
+﻿// ----------------------------------------------------------------------- 
+// <copyright file="Program.cs" company="FH Wiener Neustadt"> 
+// Copyright (c) Gunter Wiesinger. All rights reserved. 
+// </copyright> 
+// <summary>Represents the Program.cs class.</summary> 
+// <author>Gunter Wiesinger/Auto generated</author> 
+// -----------------------------------------------------------------------
+namespace ExecutableTransmitter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents the Program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The entry point of the application.
+        /// </summary>
+        /// <param name="args">The cla's in an array.</param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey(true);
+        }
+    }
+}
